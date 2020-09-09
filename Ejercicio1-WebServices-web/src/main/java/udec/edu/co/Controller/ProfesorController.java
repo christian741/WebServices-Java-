3cf8ddea0a1d3ac9c8c1666bda870b08ae5de397
christian741 <christian.xd.417@gmail.com>
2020-09-09 10:03:03 -0500

    servicios

---
+++ Ejercicio1-WebServices-web/src/main/java/udec/edu/co/Controller/ProfesorController.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udec.edu.co.Controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import udec.edu.co.Logica.ProfesorService;
+import udec.edu.co.Pojo.Profesor;
+
+/**
+ *
+ * @author Christian
+ */
+@Stateless
+@Path("/profesores")
+public class ProfesorController {
+
+    @Path("/insertar")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response insertarProfesor(ArrayList<Profesor> profesor) throws IOException {
+        ProfesorService service = new ProfesorService();
+        service.insertarProfesor(profesor);
+        return Response.status(Response.Status.CREATED).build();
+        //201 create si esta bien
+        //mal 404
+        //base datos 500
+    }
+
+    @Path("/editar")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response editar(Profesor profesor) {
+        System.out.println(profesor.getCedula() + " " + profesor.getNombre());
+        ProfesorService service = new ProfesorService();
+        service.editarProfesor(profesor);
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @Path("/retornarPorCedula/{cedula}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retornarProfesorPorCedula(@PathParam("cedula") long cedula) {
+
+        ProfesorService service = new ProfesorService();
+        Profesor Profesor = service.retornarProfesorPorCedula(cedula);
+        return Response.status(Response.Status.OK).entity(Profesor).build();
+        //filtros vacio no encontrado 404 no content
+        // 400 validaciones en solicitudes mal hechas
+    }
+    
+    @Path("/retornarMateria/{materia}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retornarProfesorPorMateria(@PathParam("materia") String materia) {
+
+        ProfesorService service = new ProfesorService();
+         System.out.println(materia);
+        ArrayList<Profesor> lista = service.buscarProfesoresMateria(materia);
+        return Response.status(Response.Status.OK).entity(lista).build();
+        //filtros vacio no encontrado 404 no content
+
+    }
+    
+    @Path("/retornarTodos")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retornarProfesorPorCedula() {
+
+        ProfesorService service = new ProfesorService();
+        ArrayList<Profesor> lista = service.retornarProfesores();
+        return Response.status(Response.Status.OK).entity(lista).build();
+        // vacia 204 no content
+
+    }
+    
+    
+    @Path("eliminar/{cedula}")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response eliminar(@PathParam("cedula") long cedula) {
+        //Lógica de base de datos
+        ProfesorService service = new ProfesorService();
+        service.eliminarProfesor(cedula);
+        return Response.status(Response.Status.NO_CONTENT).build();
+        //204 no content
+        //404 not found
+    }
+
+   
+    
+    
+
+}
---
+++ Ejercicio1-WebServices-web/src/main/java/udec/edu/co/Pojo/Profesor.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udec.edu.co.Pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+/**
+ *
+ * @author Christian y Cristian
+ */
+public class Profesor implements Serializable {
+
+    private static final long serialVersionUID=-1L;
+    
+    private long cedula;
+
+    private String nombre;
+
+    private String apellido;
+
+    private int edad;
+
+    private List<String> listaMateria;
+
+
+
+    public Profesor() {
+
+    }
+
+    public Profesor(long cedula, String nombre, String apellido, int edad) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    
+    public Profesor(long cedula, String nombre, String apellido, int edad, List<String> listaMateria) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.listaMateria = listaMateria;
+     
+    }
+
+    public long getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(long cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public List<String> getListaMateria() {
+        return listaMateria;
+    }
+
+    public void setListaMateria(List<String> listaMateria) {
+        this.listaMateria = listaMateria;
+    }
+
+   
+
+}
